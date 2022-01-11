5e77c2939c6dad9dc69a9d9c667c746ccc7529a0
jean torres <j.torres@mycoachsport.com>
2022-01-11 10:06:15 +0100

    fix dependence

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
 
     ],
     dependencies: [
-        .package(name: "Bolts", url: "https://github.com/drdaz/Bolts-ObjC", .branch("master"))
+        .package(name: "Bolts", url: "https://github.com/GlobalSport/Bolts-ObjC", .branch("master"))
 //        .package(name: "Bolts",
 //                 url: "https://github.com/drdaz/Bolts-ObjC",
 //                 .revision("df0c47add16f6cb7e81fd28aa59518c607a9dd4e"))
